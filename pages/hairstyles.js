68e4b1c52ac5fc3b818e699d93a5a212e2881fac
Britnay Gore <britnaygore268@gmail.com>
2024-06-08 13:24:58 -0400

    Can search on all hairstyle page

--- pages/hairstyles.js
+++ pages/hairstyles.js
@@ -1,23 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import HairstyleCard from '../components/HairstyleCard';
 import { getPublicHairstyle } from '../api/mergedData';
+import SearchBar from '../components/SearchBar';
 
 export default function ShowAllHairstlyes() {
   const [hairstyles, setHairstyles] = useState();
+  const [searchResults, setSearchResults] = useState([]);
 
-  const getAllTheHairstyles = () => {
-    getPublicHairstyle().then(setHairstyles);
+  const getAllTheHairstyles = async () => {
+    const fetchedHairstyles = await getPublicHairstyle();
+    setHairstyles(fetchedHairstyles);
+    setSearchResults(fetchedHairstyles);
   };
 
   useEffect(() => {
     getAllTheHairstyles();
   }, []);
 
+  const searchItems = (query) => {
+    // If search value is empty, show all public hairstyles
+    if (!query) {
+      getAllTheHairstyles();
+    } else {
+      const filtered = hairstyles.filter((hairstyle) => hairstyle.name.toLowerCase().includes(query));
+      setSearchResults(filtered);
+    }
+  };
+
   return (
     <>
+      <SearchBar onSearch={searchItems} />
       <div className="hairstyle-page">
-        {hairstyles?.length === 0 ? <h1 style={{ color: 'white', textAlign: 'center', width: '100%' }}>You Have No Hairstyles</h1> : hairstyles?.map((hairstyle) => (
-          <HairstyleCard key={hairstyle.firebaseKey} hairstyleObj={hairstyle} onUpdate={getAllTheHairstyles} />
+        {searchResults?.length === 0 ? <h1 style={{ color: 'white', textAlign: 'center', width: '100%' }}>You Have No Hairstyles</h1> : searchResults?.map((hairstyleObject) => (
+          <HairstyleCard key={hairstyleObject.firebaseKey} hairstyleObj={hairstyleObject} onUpdate={getAllTheHairstyles} />
         ))}
       </div>
     </>
