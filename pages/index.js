571a33051048232d2eaf46a23da035f156b65600
Britnay Gore <britnaygore268@gmail.com>
2024-06-21 16:37:29 -0400

    Remove duplicate Navbar and editted home page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@
 import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
 
 function Home() {
-  const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  const { user } = useAuth();
 
   return (
     <div
@@ -11,11 +11,12 @@ function Home() {
       style={{
         height: '90vh',
         padding: '30px',
-        maxWidth: '400px',
+        // maxWidth: '400px',
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.displayName}! </h1>
+      <h1>Welcome {user.displayName} to StyleStac! </h1>
+      <h5 style={{ color: 'whitesmoke' }}>To get the most out of the app, we recommend watching this quick video below for a walkthrough of the features.</h5>
     </div>
   );
 }
